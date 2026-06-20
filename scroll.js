69e79e0bca998df408cc001ae5b978c60f02cab8
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-06-20 16:48:16 +0100

    Add @updateURL

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dragon Cave - Scroll Reload
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Reload Scroll Every 30 Minutes
 // @author       kakol20
 // @match        https://dragcave.net/dragons
@@ -9,6 +9,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dragcave.net
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/kakol20/Dragon-Cave-Tampermonkey/main/scroll.js
+// @updateURL  https://raw.githubusercontent.com/kakol20/Dragon-Cave-Tampermonkey/main/scroll.js
 // ==/UserScript==
 
 (function() {
